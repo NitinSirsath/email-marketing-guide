3c7f5e7a8537223917e0a9526ac6abdb8a0b8c6e
Nitin Sirsath <73011138+NitinSirsath@users.noreply.github.com>
2025-01-12 11:19:50 +0530

    replace with live url

--- src/services/api/urls.ts
+++ src/services/api/urls.ts
@@ -1,3 +1,4 @@
 export const urls = {
-  baseURL: "http://localhost:8000/api/",
+  // baseURL: "http://localhost:8000/api/",
+  baseURL: "https://email-marketing-backend-wqez.onrender.com/api/",
 };
